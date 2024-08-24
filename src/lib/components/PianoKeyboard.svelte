9f127f5ee0c68fda1c81eff81dee5d75705b956a
Tim <tima@nopublic.ru>
2024-08-24 21:42:06 +0300

    PianoKeyboard component

---
+++ src/lib/components/PianoKeyboard.svelte
@@ -0,0 +1,198 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import * as Tone from 'tone'
+
+	// navigator.permissions.query({
+	// 	// @ts-ignore
+	// 	name: 'midi',
+	// 	sysex: true
+	// }).then(result => {
+	// 	alert(result.state)
+	// })
+
+	// let midi: any
+
+	// $: console.log(midi)
+
+	// navigator.requestMIDIAccess().then((ma) => midi = ma)
+
+	let { showKeys = $bindable(false) }: { showKeys?: boolean } = $props()
+
+	const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] as const
+
+	const urls: Record<string, string> = {
+		'A0': 'A0.mp3',
+		'C8': 'C8.mp3',
+	}
+
+	for (let note of ['C', 'D#', 'F#', 'A']) {
+		for (let octave = 1; octave <= 7; octave++) {
+			urls[note + octave] = note.replace('#', 's') + octave + '.mp3'
+		}
+	}
+
+	const sampler = new Tone.Sampler({
+		urls,
+		release: 1,
+		baseUrl: "https://tonejs.github.io/audio/salamander/"
+	}).toDestination()
+
+	function playNote(note: string) {
+		sampler.triggerAttackRelease(note, '0.2n');
+	}
+
+	let pressedNotes: string[] = $state([])
+
+	const keyToNoteMap: Record<string, string> = {
+		a: 'A3',
+		w: 'A#3',
+		s: 'B3',
+		d: 'C4',
+		r: 'C#4',
+		f: 'D4',
+		t: 'D#4',
+		g: 'E4',
+		h: 'F4',
+		u: 'F#4',
+		j: 'G4',
+		i: 'G#4',
+		k: 'A4',
+		o: 'A#4',
+		l: 'B4',
+		';': 'C5',
+		'[': 'C#5',
+		'\'': 'D5',
+		']': 'D#5',
+		'\\': 'E5'
+	}
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (keyToNoteMap[e.key] && !pressedNotes.includes(keyToNoteMap[e.key])) {
+			pressedNotes.push(keyToNoteMap[e.key])
+			sampler.triggerAttack(keyToNoteMap[e.key])
+		}
+	}
+
+	function handleKeyUp(e: KeyboardEvent) {
+		pressedNotes = pressedNotes.filter(n => n !== keyToNoteMap[e.key])
+		sampler.triggerRelease(keyToNoteMap[e.key])
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', handleKeyDown)
+		window.addEventListener('keyup', handleKeyUp)
+
+		return () => {
+			window.removeEventListener('keyup', handleKeyUp)
+		}
+	})
+</script>
+
+<div class="keyboard" class:show-keys={showKeys}>
+	<button
+		onmousedown={() => playNote('A0')} 
+		class="white-key"
+	>A</button>
+	<button
+		onmousedown={() => playNote('A#0')} 
+		class="black-key"
+	>A#</button>
+	<button
+		onmousedown={() => playNote('B0')} 
+		class="white-key"
+	>H</button>
+	{#each [1, 2, 3, 4, 5, 6, 7] as octave}
+		{#each notes as note}
+			<button
+				onmousedown={() => playNote(note + octave)} 
+				class:active={pressedNotes.includes(note + octave)}
+				class:white-key={note.length === 1}
+				class:black-key={note.length === 2}
+			>{note}</button>
+		{/each}
+	{/each}
+	<button
+		onclick={() => playNote('C8')} 
+		class="white-key"
+	>C</button>
+</div>
+
+<style lang="scss">
+	.keyboard {
+		display: flex;
+		// position: absolute;
+		// bottom: 0;
+		// left: 50%;
+		// transform: translateX(-50%);
+
+		// background: #101011;
+		// padding: 0.25rem;
+		
+		border-radius: 0.5rem;
+		width: fit-content;
+		overflow-x: scroll;
+		user-select: none;
+		-ms-overflow-style: none;  /* IE and Edge */
+  		scrollbar-width: none;  /* Firefox */
+
+		&.show-keys {
+			.white-key, .black-key {
+				color: gray;
+			}
+		}
+
+		::-webkit-scrollbar {
+			display: none;
+		}
+	}
+
+	.white-key {
+		box-shadow: 0px 4px 18px rgb(0 0 0/4%),0px 2.025px 7.84688px rgba(0,0,0,.027),0px 0.8px 2.925px rgb(0 0 0/2%),0px 0.175px 1.04062px rgba(0,0,0,.013);
+		background: linear-gradient(#E5E5E5, white);
+		height: 12.627rem;
+		width: 2.215rem;
+		border-radius: 0.25rem;
+		border: none;
+		box-shadow: inset 0 -0.5rem 2px lightgray;
+		transition: 0.2s;
+		cursor: pointer;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-end;
+		padding-bottom: 1rem;
+		color: transparent;
+
+		&:active, &.active {
+			background: linear-gradient(#E5E5E5, #E5E5E5);
+			box-shadow: inset 0 0rem 2px gray;
+			padding-bottom: 0.5rem;
+		}
+	}
+
+	.black-key {
+		width: 1.37rem;
+		height: 8rem;
+		background: linear-gradient(black, #333);
+		border-radius: 0.175rem;
+		margin: 0 calc(-1 * 1.37rem / 2);
+		z-index: 1;
+		border: none;
+		box-shadow: inset 0 -0.5rem 2px black;
+		transition: 0.2s;
+		cursor: pointer;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-end;
+		padding-bottom: 0.5rem;
+		font-size: 0.75rem;
+		color: transparent;
+
+		&:active, &.active {
+			background: linear-gradient(black, black);;
+			box-shadow: inset 0 0rem 2px black;
+			padding-bottom: 2px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import PianoKeyboard from '$lib/components/PianoKeyboard.svelte'
+
+	let showKeys = $state(false)
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={showKeys}>
+	Show keys
+</label>
+
+<PianoKeyboard {showKeys}/>
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	:global(body) {
+		font-family: sans-serif;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		gap: 1rem;
+		align-items: center;
+		justify-content: flex-end;
+		background: #efdac9;
+		padding: 2rem 0;
+		overflow-y: hidden;
+	}
+</style>
